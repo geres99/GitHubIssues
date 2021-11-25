9da6b5d694639a2a101974bef7d03a014003d39f
Geres99 <medryk.kontakt@gmail.com>
2021-11-25 23:06:02 +0100

    config fixes

--- .storybook/fonts.ts
+++ .storybook/fonts.ts
@@ -3,21 +3,21 @@ import { createGlobalStyle } from "styled-components";
 export default createGlobalStyle`
   @font-face {
       font-family: 'sourceSansProBold';
-      src: url("/fonts/sourcesanspro-bold.ttf");
+      src: url("/fonts/SourceSansPro-Bold.ttf");
       font-weight: normal;
       font-style: normal;
       font-display: swap;
   }
   @font-face {
     font-family: 'sourceSansProSemiBold';
-    src: url("/fonts/sourcesanspro-semibold.ttf");
+    src: url("/fonts/SourceSansPro-SemiBold.ttf");
     font-weight: normal;
     font-style: normal;
     font-display: swap;
   }
   @font-face {
       font-family: 'sourceSansProRegular';
-      src: url("/fonts/sourcesanspro-regular.ttf");
+      src: url("/fonts/SourceSansPro-Regular.ttf");
       font-weight: normal;
       font-style: normal;
       font-display: swap;
